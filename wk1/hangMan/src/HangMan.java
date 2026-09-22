e21a2f29653d3e956e8f56ca5b094fb68a1fbfc7
thalesmf05 <thalesferrari5@gmail.com>
2026-09-22 16:45:40 +0100

    Implement input validation and improve game logic for HangMan

--- wk1/hangMan/src/HangMan.java
+++ wk1/hangMan/src/HangMan.java
@@ -1,18 +1,20 @@
 import java.util.Objects;
 
 public class HangMan {
-    private String sentence, revelingSentence, letter;
-    private int numWord, numLetters;
-    private String [] words;
+    private String sentence, revelingSentence, letter, errorHandling;
+    private int numWord, numLetters, countCorrectGuess;
     private char lt;
     private StringBuilder sb = new StringBuilder();
-    private boolean correctGuess = false;
+    private boolean correctGuess = false, isletterValid;
     //constructor
     public HangMan() {
         sentence = "";
         revelingSentence= "";
+        letter = "";
         numWord = 0;
         numLetters = 0;
+        countCorrectGuess = 0;
+        isletterValid = false;
     }
 
     //getters and setters
@@ -40,16 +42,27 @@ public class HangMan {
         this.letter = letter;
     }
 
+    public int getCountCorrectGuess() {
+        return countCorrectGuess;
+    }
+
+    public boolean isIsletterValid() {
+        return isletterValid;
+    }
+
+    public String getErrorHandling() {
+        return errorHandling;
+    }
+
     //compute main
     public void startGame(){
         numWord = sentence.strip().split("\\s+").length; //count words
         for(int i = 0; i < sentence.length(); i++){
             if(Character.isLetter(sentence.charAt(i))){
                 numLetters++; //count letter
-
                 //create revelingSentence
                 revelingSentence = revelingSentence + "_";
-            } else if (lt == ' ') {
+            } else{
                 revelingSentence = revelingSentence + " ";
             }
         }
@@ -62,14 +75,27 @@ public class HangMan {
             if(letter.equals(Character.toString(sentence.charAt(i)))){
                 sb.replace(i, i + 1, letter);
                 correctGuess = true;
+                countCorrectGuess++; //count the number of correct guessed letters
             }
         }
         if(correctGuess){
             revelingSentence = sb.toString();
         }
         sb.setLength(0); //clen the string builder for the next word
-
     }
+
+
+    //VALIDATION LETTER AND SENTENCE
+    public void validadeLetterInput(){
+        if(letter.length() != 1){
+            errorHandling = "Only one letter at the time";
+        }else if(Character.isLetter(letter.charAt(0))){
+            isletterValid = true;
+        }else{
+            errorHandling = "Only letters are accepted";
+        }
+    }
+
 
 
 }
--- wk1/hangMan/src/HangManApp.java
+++ wk1/hangMan/src/HangManApp.java
@@ -2,40 +2,56 @@ import javax.swing.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TODO fix bug with spaces, verification class (first input), display after winning, count the lost points
+
 public class HangManApp {
     public static void main(String[] args) {
-        String sentence, revelingSentence, letter;
-        Boolean isFinished = false, correctGuess;
+        String sentence, revelingSentence, letter, errorHandling;
+        Boolean isFinished = false, correctGuess, isLetterValid = false;
+        int countWord, countLetter, countCorrectGuess = 0;
         HangMan myHm = new HangMan();
 
-        //ask for input
+        //INPUT
         sentence = JOptionPane.showInputDialog(null, "Enter the word: ");
         myHm.setSentence(sentence);
-        myHm.startGame();
 
-        while(!isFinished){
-            //show input
+        myHm.startGame(); //START THE GAME
 
+        //Get relevant info to display for the second user
+        countWord = myHm.getNumWord();
+        countLetter = myHm.getNumLetters();
+        revelingSentence = myHm.getRevelingSentence();
 
+        JOptionPane.showMessageDialog(null, "Words: " + countWord + "\nLetters: " + countLetter
+                + "\n" +
+                "Hint: " + revelingSentence);
+
+        while(countCorrectGuess < countLetter){
            //get the guess
            letter = JOptionPane.showInputDialog(null, "Enter a letter: ");
-           myHm.setLetter(letter);
 
-           //compute guess letter
+           //validate letter input
+            myHm.setLetter(letter);
+            myHm.validadeLetterInput();
+
+            if (!myHm.isIsletterValid()){ //if not valid show error message and restart
+                errorHandling = myHm.getErrorHandling();
+                JOptionPane.showMessageDialog(null, errorHandling);
+                continue;
+            }
+
+            //play the round
             myHm.guessLetter();
 
             correctGuess = myHm.isCorrectGuess();//check if user got a word
             if(correctGuess){
                 revelingSentence = myHm.getRevelingSentence();
-                JOptionPane.showMessageDialog(null, "You got it! \nCurrent word: " + revelingSentence);
+                JOptionPane.showMessageDialog(null, "You got it! \nCurrent word: \n" + revelingSentence);
+                countCorrectGuess = myHm.getCountCorrectGuess();
             }
-
-
-            revelingSentence = myHm.getRevelingSentence();
-            JOptionPane.showMessageDialog(null, revelingSentence);
         }
 
-
-        //sendit for the other page
+        //won the game
+        JOptionPane.showMessageDialog(null, "won");
     }
 }
